3429df3fefc62d15d5e2e7950b86ed68caddd3f5
Nicolas <nicolasreedaugusto@gmail>
2026-07-23 21:47:42 +0300

    Made some modifications to Human Choice and finished Score Keeping

--- RockPaperScisors.js
+++ RockPaperScisors.js
@@ -1,15 +1,18 @@
     // Human choice
-let computerAnswer = "none"
+let computerChoice = "none"
 let humanAnswer = "none"
 function getHumanChoice() {
     
     humanAnswer = prompt("Rock, Paper, or Scissors?")
+    humanAnswer = humanAnswer.toUpperCase()
+    humanAnswer = humanAnswer.charAt(0).toUpperCase() + humanAnswer.slice(1).toLowerCase();
     return humanAnswer
+
 }
 
-console.log(getHumanChoice())
 
-
+humanChoice = getHumanChoice()
+console.log(humanAnswer)
 
     // Computer's Choice
 
@@ -19,19 +22,62 @@ function getComputerChoice() {
 }
 // Rock
 if (getComputerChoice() < 10) {
-    computerAnswer = "Rock"
+    computerChoice = "Rock"
 
 }
 //Paper
 else if (getComputerChoice() >= 10 && getComputerChoice() < 20) {
 
-    computerAnswer = "Paper"
+    computerChoice = "Paper"
 
 }
 // Scissors
-else 
+else {    
+    computerChoice = "Scissors"
+}
+
+console.log(computerChoice)
+
+
+    // Score keeping
+let humanScore = 0
+let computerScore = 0
+if (humanChoice == computerChoice) {
     
-    computerAnswer = "Scissors"
+    console.log("It's a tie!")
+    humanScore += 0.5
+    computerScore += 0.5
 
+}
 
+if (humanChoice == "Rock" && computerChoice == "Paper") {
+    console.log("You Lose!")
+    computerScore += 1 
+}
 
+if (humanChoice == "Rock" && computerChoice == "Scissors") {
+    console.log("You Win!")
+    humanScore += 1 
+}
+
+if (humanChoice == "Paper" && computerChoice == "Rock") {
+    console.log("You Win!")
+    humanScore += 1 
+}
+
+if (humanChoice == "Paper" && computerChoice == "Scissors") {
+    console.log("You Lose!")
+    computerScore += 1 
+}
+
+if (humanChoice == "Scissors" && computerChoice == "Rock") {
+    console.log("You Lose!")
+    computerScore += 1 
+}
+if (humanChoice == "Scissors" && computerChoice == "Paper") {
+    console.log("You Win!")
+    humanScore += 1 
+}
+
+console.log (humanScore)
+console.log (computerScore)
